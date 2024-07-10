a85525bb8db5c9860190ffb21a861ee412b96fae
George Ngigi <ngigegeorge023@gmail.com>
2024-07-10 13:08:39 +0300

    Moved all static from within app to root. Added contact and about page.

--- Aquareach/settings.py
+++ Aquareach/settings.py
@@ -140,6 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, "node_modules"),
+]
 
 
 MEDIA_URL = "/media/"
